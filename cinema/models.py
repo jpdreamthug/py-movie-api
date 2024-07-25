c3d0bacd51578df0e40f617ffac0785988070b2d
jpdreamthug <yaroslavkondrattsev@gmail.com>
2024-07-25 13:41:40 +0300

    add solution

--- cinema/models.py
+++ cinema/models.py
@@ -4,4 +4,7 @@ from django.db import models
 class Movie(models.Model):
     title = models.CharField(max_length=255)
     description = models.TextField()
-    duration = models.TimeField()
+    duration = models.IntegerField()
+
+    def __str__(self):
+        return self.title
--- movie_api/settings.py
+++ movie_api/settings.py
@@ -38,6 +38,8 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     "cinema",
+    "rest_framework",
+
 ]
 
 MIDDLEWARE = [
